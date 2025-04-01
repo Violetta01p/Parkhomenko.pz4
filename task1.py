aff5346f2dbcbb26bfea921499957f8662d8d98f
Violetta01p <pvsergeevnaa@gmail.com>
2025-04-01 12:49:29 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+a
+ge = float(input("Enter your age: "))
+city = input("Enter your city: ")
+formatted_text = f"Name: {first_name:<10} Age: {age:.2f} City: {city:>15}"
+print(formatted_text)
